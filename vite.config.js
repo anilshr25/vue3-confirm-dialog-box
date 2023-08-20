37f5aa1d579f5839b96074c488a9488b6e01fa20
Anil Shrestha <31877452+anilshr25@users.noreply.github.com>
2023-08-20 09:50:57 +0545

    fix css

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { resolve } from "path"
 import { defineConfig } from "vite";
 import createVuePlugin from "@vitejs/plugin-vue";
-
+import libCss from 'vite-plugin-libcss';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [
+    createVuePlugin(),
+    libCss()
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.js"),
